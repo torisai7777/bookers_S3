3b4ebe3c902f8e3680a586918da031cff905c820
torisai7777 <torii.saito.infratop@gmail.com>
2023-10-11 08:46:40 +0000

    7c

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
+  has_many :group_users
+  has_many :groups, through: :group_users
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
